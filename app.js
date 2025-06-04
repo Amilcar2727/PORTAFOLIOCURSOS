3356291accd39f36086f1f22be4ea1d0278ec51d
Amilcar <bambino.amil.27@gmail.com>
2025-06-04 13:09:16 -0500

    Borrado de nodemoon, prueba basica de javascript, y integracion css

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 const app = express();
 app.set("view engine","ejs")
 
+// Servir arhivos estáticos desde /Public
+app.use(express.static("public"));
 // Middleware para poder leer JSON o formularios
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +16,9 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("✅ Conexión a MongoDB exitosa"))
 .catch(err => console.error("❌ Error al conectar a MongoDB:", err));
 
+
 /*MongoDB*/
+//Crear nuevo usuario
 const Usuario = require("./models/Usuario")
 app.get("/crear-usuario",async (req,res)=>{
     const nuevoUsuario = new Usuario({
@@ -29,21 +33,25 @@ app.get("/crear-usuario",async (req,res)=>{
     console.log("✅ Usuario creado y guardado en MongoDB");
 })
 /*Obtener paginas web*/
-app.get("/Portafolio",(req,res)=>{
-    Usuario.find({}).
-    then(function(users){
-        res.render("test",{usuarios: users});
-    }).catch(err=>{
+app.get("/Usuarios",async (req,res)=>{
+    try{
+        const usuarios = await Usuario.find();
+        res.render("test", { usuarios });
+    }catch(err){
         console.log(err);
         res.status(500).send("Error al cargar la pagina");
-    });
+    };
 })
-app.get("/Test",(req,res)=>{
-    res.render("test");
+app.get("/",(req,res)=>{
+    res.render("index");
 })
-
-//EJS -> Template engine - Node
 
 app.listen(3000, (req,res)=>{
-    console.log("Corriendo desde el puerto 3000");
+    console.log("🚀 Servidor corriendo en http://localhost:3000");
 })
+
+// 💥 Manejo de cierre controlado
+process.on('SIGINT', async () => {
+    await mongoose.connection.close();
+    console.log('\n🛑 Conexión a MongoDB cerrada por Ctrl+C');
+});
