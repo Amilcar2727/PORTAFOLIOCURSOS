6b334826c00d035e321e316e12358e246a3696af
Amilcar <bambino.amil.27@gmail.com>
2025-06-07 21:30:45 -0500

    Generacion de Portafolios

--- services/utils/generarPortafolios.js
+++ services/utils/generarPortafolios.js
@@ -1,9 +1,34 @@
-const generarCarpetasExamenes = require('./generarCarpetasExamenes');
+function generarPortafolios(usuarios, portafoliosCursos, semestre) {
+    const portafolios = [];
 
-function generarPortafolios(usuarios, asignaturas, semestre) {
-    console.log("Generando portafolios para los usuarios...");
-    const examenes = generarCarpetasExamenes(asignaturas);
-    return;
+    console.log("🗂 Generando portafolios...");
+    usuarios.forEach(usuario => {
+        // Filtrar portafoliosCursos cuyo asignaturaId incluye el usuario._id (es lo que pusiste al final del id)
+        const cursosDelDocente = portafoliosCursos.filter(curso => {
+            return curso.asignaturaId.includes(usuario.codigo);
+        });
+
+         // Extraer el id del curso, asignaturaId simula el id del curso
+        const cursosIds = cursosDelDocente.map(curso => curso.asignaturaId);
+
+        // Crear el portafolio
+        const nuevoPortafolio = {
+            docenteId: usuario.codigo,
+            semestre: semestre,
+            creadoEn: new Date(),
+            presentacion: {
+                caratula: [],
+                cargaAcademica: [],
+                filosofiaDoc: [],
+                cv: []
+            },
+            cursos: cursosIds
+        };
+
+        portafolios.push(nuevoPortafolio);
+    });
+
+    return portafolios;
 }
 
 module.exports = generarPortafolios;
