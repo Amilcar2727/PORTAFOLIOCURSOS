2da262d48b4d6fb59ccd98ba222be05f8814e477
Amilcar <bambino.amil.27@gmail.com>
2025-06-10 07:15:31 -0500

    Implementacion pagina principal

--- app.js
+++ app.js
@@ -1,102 +1,42 @@
 const express = require("express");
-const mongoose = require("mongoose");
 require("dotenv").config();
-const multer = require("multer");
-const path = require("path");
-const XLSX = require("xlsx");
-
+const {conectarDB, cerrarConexion} = require("./config/mongoose");
 // Configurar dependencias
 const app = express();
 app.set("view engine","ejs");
-const storage = multer.memoryStorage();
-const upload = multer({storage: storage});
 
-app.post("/subir_excel",upload.single("archivoExcel"),(req, res)=>{
-    // Comprobar que el archivo existe 
-    if(!req.file) return res.status(400).send("No se subió ningun archivo");
-    // Procesar archivo excel en memoria
-    const buffer = req.file.buffer;
-    // Leer workbook con xlsx
-    const workbook = XLSX.read(buffer, {type: "buffer"});
-
-    // Asumiendo que queremos la primera hoja
-    const sheetName = workbook.SheetNames[0];
-    const worksheet = workbook.Sheets[sheetName];
-    
-    // Convertir hoja a JSON
-    const datos = XLSX.utils.sheet_to_json(worksheet);
-
-    // datos => arreglo de objetos con las filas
-    console.log(datos);
-
-    /* Aqui podemos procesar los datos para crear portafolios */
-    //Ex: res.json(datos);
-
-    res.send("Archivo procesado con exito");
-})
-
-
-// Servir arhivos estáticos desde /Public
-app.use(express.static("public"));
-// Middleware para poder leer JSON o formularios
-app.use(express.json());
+// Middleware global
+app.use(express.static("public")); // Servir arhivos estáticos desde /Public
+app.use(express.json());    // Para poder leer JSON o formularios
 app.use(express.urlencoded({ extended: true }));
 
-// Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("✅ Conexión a MongoDB exitosa"))
-.catch(err => console.error("❌ Error al conectar a MongoDB:", err));
+// Conectar a MongoDB
+conectarDB();
 
-/* PAGINAS */
-//Page -> Crear nuevo usuario
-const Usuario = require("./models/Usuario_Schema")
-app.get("/crear-usuario",async (req,res)=>{
-    if (mongoose.connection.readyState !== 1) {
-        return res.status(500).send("❌ Error: No hay conexión al Servidor.");
-    }
-    const nuevoUsuario = new Usuario({
-        codigo: "200822",
-        nombre: "Amilcar Estacio",
-        correo: "bambino.amil.27@gmail.com",
-        rol: "Evaluador",
-        contraseña: "123456"
-    });
-    await nuevoUsuario.save();
-    res.send("✅ Usuario creado y guardado en MongoDB");
-    console.log("✅ Usuario creado y guardado en MongoDB");
-})
-//Page -> Listar Usuarios
-app.get("/Usuarios",async (req,res)=>{
-    if (mongoose.connection.readyState !== 1) {
-        return res.status(500).send("❌ Error: No hay conexión al Servidor.");
-    }
-    try{
-        const usuarios = await Usuario.find();
-        res.render("usuarios_page.ejs", { usuarios });
-    }catch(err){
-        console.log(err);
-        res.status(500).send("Error al cargar la pagina");
-    };
-})
-//Page -> Subir Catalogo
-app.get("/Subir_Portafolios",(req,res)=>{
-    res.render("subir_portafolios");
-})
+const usuarioRoutes = require("./routes/usuario/usuario.routes");
+const adminRoutes = require("./routes/admin");
 
-//Page -> Main Page
-app.get("/",(req,res)=>{
+app.use("/admin",adminRoutes);
+app.use("/usuarios",usuarioRoutes);
+
+// ==>> Pagina Principal <<==
+app.get("/", (req, res) => {
     res.render("index.ejs");
-})
+});
 
-app.listen(3000, (req,res)=>{
+//Iniciar el servidor
+const server = app.listen(3000, () => {
     console.log("🚀 Servidor corriendo en http://localhost:3000");
-})
+});
+
+
+//Para matar procesos zombies
+//netstat -ano | findstr :3000
+//taskkill /PID 9840 /F
 
 // Manejo de cierre controlado
 process.on('SIGINT', async () => {
-    console.log('\n🛑 Cerrando servidor...')
-    await mongoose.connection.close()
-        .then(()=>console.log('\n🛑 Conexión a MongoDB cerrada'))
-        .catch(err => console.log("⚠ Error cerrando MongoDB:", err))
-    process.exit(0);
+    console.log('\n🛑 Cerrando servidor...');
+    await cerrarConexion();
+    server.close(() => process.exit(0));
 });
